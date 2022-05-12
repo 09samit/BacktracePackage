8ff982d44b5173830bae3c3f34de91260cd6f88e
Amit Garg <09s.amitgarg@gmail.com>
2022-05-12 22:23:24 +0530

    update

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BacktracePackage",
-            targets: ["Backtrace"]),
+            targets: ["Backtrace", "Backtrace_PLCrashReporter"]),
     ],
     targets: [
         .binaryTarget(
